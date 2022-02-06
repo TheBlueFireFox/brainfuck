329e23caa3e8e33a4455bcfec2bdd527cff113b5
Adrian Hornung <hornung.adrian@gmail.com>
2022-02-06 22:43:34 +0100

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io::Read;
 
+#[allow(dead_code)]
 const PROGRAM_HALLO_WORLD: &str = include_str!("../input.txt");
+
+#[allow(dead_code)]
 const PROGRAM_ADDING: &str = include_str!("../input1.txt");
 
 fn main() {
@@ -13,11 +16,12 @@ struct Interpreter<'program> {
     program: &'program [u8],
     data_ptr: usize,
     cells: Vec<u8>,
-    last: Vec<(usize, Option<usize>)>,
+    last: Vec<usize>,
 }
 
 impl<'program> Interpreter<'program> {
     const SIZE: usize = 30_000;
+
     fn new<'input, T>(program: &'input T) -> Self
     where
         T: AsRef<[u8]> + 'input + ?Sized,
@@ -48,47 +52,39 @@ impl std::iter::Iterator for Interpreter<'_> {
             b'<' => self.data_ptr -= 1,
             b'+' => self.cells[self.data_ptr] += 1,
             b'-' => self.cells[self.data_ptr] -= 1,
-            b'.' => {
-                print!("{}", char::from_u32(self.cells[self.data_ptr] as _)?)
-            }
-            b',' => {
-                let next = std::io::stdin().bytes().next()?.ok()?;
-
-                self.cells[self.data_ptr] = next;
-            }
+            b'.' => print!("{}", char::from_u32(self.cells[self.data_ptr] as _)?),
+            b',' => self.cells[self.data_ptr] = std::io::stdin().bytes().next()?.ok()?,
             b'[' => {
-                if self.cells[self.data_ptr] == 0 {
-                    // jump to ]
-                    let mut idx = 0;
-                    loop {
-                        idx += match self.program[self.prog_ptr] {
-                            b']' => -1,
-                            b'[' => 1,
-                            _ => 0,
-                        };
-                        self.prog_ptr += 1;
-
-                        if idx == 0 {
-                            break;
-                        }
-                    }
-                } else {
-                    self.last.push((self.prog_ptr, None));
+                // find next ]
+                let mut idx = 1;
+                let mut next = self.prog_ptr;
+                while idx > 0 {
+                    next += 1;
+                    idx += match self.program[next] {
+                        b']' => -1,
+                        b'[' => 1,
+                        _ => 0,
+                    };
                 }
+                self.last.push(self.prog_ptr);
+
+                // jump to ]
+                self.prog_ptr = next - 1;
             }
             b']' => {
                 if self.cells[self.data_ptr] != 0 {
                     // jump to [
-                    let mut ptr = self.last.last_mut()?;
-                    ptr.1 = Some(self.prog_ptr);
-                    self.prog_ptr = ptr.0;
+                    let &ptr = self.last.last()?;
+                    self.prog_ptr = ptr;
                 } else {
-                    self.last.pop();
+                    let _ = self.last.pop();
                 }
             }
             _ => {}
         }
+
         self.prog_ptr += 1;
+
         Some(())
     }
 }
